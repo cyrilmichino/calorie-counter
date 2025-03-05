1f26f17f32b0fdf8db9ad438cf6ac35474c627e6
cyrilmichino <cyrilmichino@gmail.com>
2025-03-06 00:49:18 +0300

    Add meals to local storage

--- main.js
+++ main.js
@@ -12,3 +12,23 @@ function closeMenu() {
     menuOverlay.style.display = 'none'
     navbar.style.right = '100vh';
 }
+
+function addMeal() {
+    const meal = document.getElementById('description')
+    let meals = JSON.parse(localStorage.getItem('calorie-app-meals'))
+
+    if (meals == null || meals.length == 0)  {
+        localStorage.setItem('calorie-app-meals', JSON.stringify([meal]))
+    } else {
+        meals.push(meal.value)
+        localStorage.setItem('calorie-app-meals', JSON.stringify(meals))
+    }
+
+    meal.value = ""
+    console.log(JSON.parse(localStorage.getItem('calorie-app-meals')))
+}
+
+// meals = ["1/2kg goat meat and cornmeal", "4 slices of bread and tea with milk"]
+// localStorage.setItem('calorie-app-meals', JSON.stringify(meals))
+
+// console.log(JSON.parse(localStorage.getItem('calorie-app-meals')))
